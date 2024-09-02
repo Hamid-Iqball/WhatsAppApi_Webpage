cd700d96c7c0e2f27e20297588e2565f407d4318
Hamid-Iqball <hamid.iqbal00123@gmail.com>
2024-09-02 18:20:33 +0500

    completed accordian

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const accordian = document.getElementsByClassName("accordian-item")
+const accordian = document.getElementsByClassName("accordian-text")
 
 for (let i=0; i<accordian.length; i++){
     accordian[i].addEventListener("click", function(){
         this.classList.toggle("active")
-        console.log("heu")
+        console.log("hey")
     })
 }
