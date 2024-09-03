ef7ecfe11357299b6a0dd4ffc50be5f048770f85
Hamid-Iqball <hamid.iqbal00123@gmail.com>
2024-09-03 16:52:41 +0500

    final version

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const accordian = document.querySelectorAll(".accordian-item")
 accordian.forEach((accordian)=>{
     const icon = accordian.querySelector(".accordian-icon")
     const content = accordian.querySelector(".accordian-text")
+    const header = accordian.querySelector(".accordian-heading")
 
     accordian.addEventListener("click" , function(){
         icon.classList.toggle("active")
         content.classList.toggle("active")
+        header.classList.toggle("active")
     })
 })
